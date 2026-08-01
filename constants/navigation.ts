a193cb104a34b596151b7747bf3c718cf21e3c40
Swami <swami.skar@gmail.com>
2026-08-01 13:26:27 +0530

    feat: add project architecture

---
+++ constants/navigation.ts
@@ -0,0 +1,35 @@
+import { NavigationItem } from "@/types/navigation";
+
+export const navigation: NavigationItem[] = [
+
+{
+label:"Home",
+href:"#home"
+},
+
+{
+label:"Solutions",
+href:"#solutions"
+},
+
+{
+label:"Services",
+href:"#services"
+},
+
+{
+label:"Technologies",
+href:"#technologies"
+},
+
+{
+label:"About",
+href:"#about"
+},
+
+{
+label:"Contact",
+href:"#contact"
+}
+
+];
---
+++ types/navigation.ts
@@ -0,0 +1,7 @@
+export interface NavigationItem {
+
+  label: string;
+
+  href: string;
+
+}
